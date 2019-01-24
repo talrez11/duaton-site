e8253b2575dfc9b0050a5b96396107df9c8b8af3
talrez <tal@angelsense.com>
2019-01-24 13:03:44 +0200

    Added font family

--- wp-content/themes/duaton/header.php
+++ wp-content/themes/duaton/header.php
@@ -15,13 +15,11 @@
     <title><?php wp_title(); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link href="https://fonts.googleapis.com/css?family=Alef:400,700&amp;subset=hebrew" rel="stylesheet">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
-		<header>
-
-		</header><!-- .site-header -->
+		<header></header><!-- .site-header -->
