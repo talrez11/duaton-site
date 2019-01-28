e15231efdd5601197dd45fe47b5282621f6c7f3e
talrez <tal@angelsense.com>
2019-01-29 00:05:09 +0200

    Added project template and styles

--- wp-content/themes/duaton/project.php
+++ wp-content/themes/duaton/project.php
@@ -1,13 +1,30 @@
 <?php
-//Template Name: General Projects
-    wp_enqueue_style('general-projects', get_stylesheet_directory_uri().'/css/general.css?vn='.THEME_VERSION, array(), true);
-    wp_enqueue_script('script', get_stylesheet_directory_uri().'/js/script.js?vn='.THEME_VERSION, array('jquery'), true);
+//Template Name: Project
+wp_enqueue_style('project-style', get_stylesheet_directory_uri().'/css/project.css?vn='.THEME_VERSION, array(), true);
+wp_enqueue_script('script', get_stylesheet_directory_uri().'/js/script.js?vn='.THEME_VERSION, array('jquery'), true);
+global $post;
+$header_image = get_field('header_image', $post->ID, true);
+$page_title = get_field('title', $post->ID, true);
+$page_desc= get_field('description', $post->ID, true);
+
 ?>
 
 <?php get_header(); ?>
 <?php include_once ('includes/menu.php');?>
 <?php include_once ('includes/about.php');?>
-<section>
+<section class="header" style="background-image: url('<?php echo $header_image; ?>');">
+    <div class="title">
+        <?php echo $page_title; ?>
+    </div>
+</section>
+
+<section class="content">
+    <div>
+        <?php echo $page_desc; ?>
+    </div>
+</section>
+
+<section class="project">
     <?php if( have_rows('column') ): ?>
         <?php while( have_rows('column') ): the_row();?>
             <div class="<?php echo get_sub_field('class')?>">
@@ -19,7 +36,7 @@
                                     $image = get_sub_field('image');
                                     $class = get_sub_field('class');
                                     $url = get_sub_field('url');
-                                ?>
+                                    ?>
                                     <a href="<?php echo $url; ?>" class="<?php echo $class; ?>" style="background-image: url('<?php echo $image; ?>');"></a>
                                 <?php endwhile; ?>
                             <?php endif; ?>
