0a5d5b5f6081245e9c667eddcbdc0875fd1d17b6
talrez <tal@angelsense.com>
2019-01-29 21:13:25 +0200

    Added ignore file

---
+++ wp-content/themes/duaton/includes/env.php
@@ -0,0 +1,4 @@
+<?php
+define('THEME_VERSION', '1.0');
+define('MAILCHIMP_API', 'f6f71cfdd1af3b950c9dc68071c22fa4-us12');
+?>
--- wp-content/themes/duaton/includes/env2.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define('THEME_VERSION', '1.0');
-	define('MAILCHIMP_API', 'f6f71cfdd1af3b950c9dc68071c22fa4-us12');
-?>
